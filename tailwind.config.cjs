f211d721ea11ca7acc98ad328678f85331c6c7cf
Juan Pablo Restrepo <jota.restrepo@sioma.app>
2023-01-23 20:42:23 -0500

    Add tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,19 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+      'dark-violet': {
+        'DEFAULT': 'hsl(278, 68%, 11%)',
+        'gradient-from': 'hsl(249, 99%, 64%)',
+        'gradien-to': 'hsl(278, 94%, 30%)'
+      },
+      'grayish-violet': {
+        'light': 'hsl(270, 3%, 87%)',
+        'dark': 'hsl(279, 6%, 55%)'
+      }
+    }
+    },
   },
   plugins: [],
 }
